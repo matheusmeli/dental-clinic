dbb054f97d21048931db4d702d6209ba05d09ad4
matheusmeli <matheus.agomes@mercadolivre.com>
2021-06-22 16:19:29 -0300

    project setup from spring initializer

---
+++ src/test/java/br/com/mercadolivre/dentalclinic/DentalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.dentalclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
